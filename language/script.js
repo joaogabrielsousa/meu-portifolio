4360d4b9b5479fb6b1bba6ebd52392e1f57c70ec
joaogabrielsousa <gabrielove11@gmail.com>
2026-04-12 02:13:02 -0300

    organizando o site
    organizei as pastas e renomeei

---
+++ language/script.js
@@ -0,0 +1,15 @@
+/*
+text-projetoLogin
+text-projetoCordel
+text-projetoAndroid
+text-projetoSocial
+*/
+
+const projetoLogin = getElementbyId("text-projetoLogin")
+const projetoCordel = getElementbyId("text-projetoCordel")
+const projetoAndroid = getElementbyId("text-projetoAndroid")
+const projetoSocial = getElementbyId("text-projetoSocial")
+
+function ()
+    
+}
